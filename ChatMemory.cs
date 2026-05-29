de8a79c90eae3ae770fd8eb4813ad0008ed3d010
SinoMkosi <166750139+SinoMkosi@users.noreply.github.com>
2026-05-29 21:07:00 +0200

    Add files via upload

---
+++ ChatMemory.cs
@@ -0,0 +1,8 @@
+﻿namespace CyberSecurityChatbot.Models
+{
+    public class ChatMemory
+    {
+        public string UserName { get; set; }
+        public string FavouriteTopic { get; set; }
+    }
+}
